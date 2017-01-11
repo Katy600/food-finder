1614c6f1e40651a6b74ea8a127f589b2f374c03c
Katy McCann <katy.alice.mccann@gmail.com>
2017-01-11 10:24:16 +0000

    Create restaurant class methods file_exists?, create_file and saved_restaurants

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,2 +1,17 @@
 class Restaurant
-end 
+
+  @@file_path = nil #class variable for the file path
+
+  def self.file_exists? #class method because it has self at the begining
+  #restaurant class should know if the restaurant file exists
+  end
+
+  def self.create_file
+    #create the restaurant file
+  end
+
+  def self.saved_restaurants
+    #read the restaurant file
+    #return instances of restaurant
+  end
+end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -1,4 +1,11 @@
 require 'restaurant'
 
 describe Restaurant do
+# context 'when checking files exist' do
+#   it { is_expected.to respond_to self.file_exists?}
+# end 
+  # it 'responds to self.file exists' do
+  #   restaurant = Restaurant.new
+  #   expect(restaurant).to respond_to file_exists?
+  # end
 end
